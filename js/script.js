3a9cadce400d0c5b98a7f5d7c10a6f4f29014c42
MattCatcher17 <198160646+MattCatcher17@users.noreply.github.com>
2026-05-22 14:24:05 +0800

    feature: add new item

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+document.body.innerHTML = `
+<div style="
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  height:100vh;
+  font-size:48px;
+  background:black;
+  color:red;
+  ">
+    SITE BROKEN
+  </div>
+`;
+
 const actionButton = document.getElementById('actionButton');
 actionButton.addEventListener('click', () => {
   alert('你按下了按鈕！');
